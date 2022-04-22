189d2f18d52032f1de78eab949cc6834b92b413b
Rishikesh <104022405+RishiSpace@users.noreply.github.com>
2022-04-22 14:53:37 +0530

    Automatically install Apps
    A program that automatically downloads and installs apps for you. Works on both Windows and Linux

--- osfm.cpp
+++ osfm.cpp
@@ -37,7 +37,10 @@ int main()
                 printf("3. terminator\n");
                 printf("4. spotify\n");
                 printf("5. chrome\n");
-                printf ("6. vscode\n");
+                printf("6. vscode\n");
+                printf("7. Steam\n");
+                printf("8. Lutris\n");
+                printf("0. All of the above\n");
                 scanf ("%d",&lp);
                 if (l == 1)
                 {
@@ -80,6 +83,35 @@ int main()
                         system ("sudo sh -c 'echo 'deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] 'https://packages.microsoft.com/repos/code stable main' > '/etc/apt/sources.list.d/vscode.list'");
                         system ("rm -f packages.microsoft.gpg");
                         system ("sudo apt install apt-transport-https && sudo apt update && sudo apt install code");
+                    }
+                    else if (lp == 7)
+                    {
+                        //steam
+                        printf("Installing Steam, please enter your passowrd if prompted\n");
+                        system ("sudo apt install steam");
+                    }
+                    else if (lp == 8)
+                    {
+                        //lutris
+                        printf("Installing Lutris, please enter your passowrd if prompted\n");
+                        system ("sudo apt install lutris");
+                    }
+                    else if (lp == 0)
+                    {
+                        //All of the above
+                        printf("Installing all programs mentioned above (This may take some time), please enter your password if prompted\n");
+                        system ("sudo apt install firefox");
+                        system ("sudo apt install neofetch");
+                        system ("sudo apt install terminator");
+                        system ("sudo apt install snap && sudo snap install spotify");
+                        system ("sudo apt install snap && sudo snap install chrome");
+                        system ("wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg");
+                        system ("sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/");
+                        system ("sudo sh -c 'echo 'deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] 'https://packages.microsoft.com/repos/code stable main' > '/etc/apt/sources.list.d/vscode.list'");
+                        system ("rm -f packages.microsoft.gpg");
+                        system ("sudo apt install apt-transport-https && sudo apt update && sudo apt install code");
+                        system ("sudo apt install steam");
+                        system ("sudo apt install lutris");
                     }
                     else
                     {
@@ -127,6 +159,31 @@ int main()
                         printf("Installing vscode, please enter your password if prompted\n");
                         system ("yay -S vscode");
                     }
+                    else if (lp == 7)
+                    {
+                        //steam
+                        printf("Installing Steam, please enter your passowrd if prompted\n");
+                        system ("yay -S steam");
+                    }
+                    else if (lp == 8)
+                    {
+                        //lutris
+                        printf("Installing Lutris, please enter your passowrd if prompted\n");
+                        system ("yay -S lutris");
+                    }
+                    else if (lp == 0)
+                    {
+                        //All of the above
+                        printf("Installing all programs mentioned above (This may take some time), please enter your password if prompted\n");
+                        system ("sudo pacman -s firefox");
+                        system ("sudo pacman -S neofetch");
+                        system ("sudo pacman -S terminator");
+                        system ("yay -S spotify");
+                        system ("yay -S chrome");
+                        system ("yay -S vscode");
+                        system ("yay -S steam");
+                        system ("yay -S lutris");
+                    }
                     else
                     {
                         printf("oops wrong choice");
@@ -141,7 +198,7 @@ int main()
                     {
                         //Firefox
                         printf("Installing Firefox, please enter your password if prompted\n");
-                        system ("sudo dnf install irefox");
+                        system ("sudo dnf install firefox");
                     }
                     else if (lp == 2)
                     {
@@ -173,6 +230,31 @@ int main()
                         printf("Installing vscode, please enter your password if prompted\n");
                         system ("flatpak install com.visualstudio.code");
                     }
+                    else if (lp == 7)
+                    {
+                        //steam
+                        printf("Installing Steam, please enter your passowrd if prompted\n");
+                        system ("sudo dnf install steam");
+                    }
+                    else if (lp == 8)
+                    {
+                        //lutris
+                        printf("Installing Lutris, please enter your passowrd if prompted\n");
+                        system ("sudo dnf install lutris");
+                    }
+                    else if (lp == 0)
+                    {
+                        //All of the above
+                        printf("Installing all programs mentioned above (This may take some time), please enter your password if prompted\n");
+                        system ("sudo dnf install firefox");
+                        system ("sudo dnf install neofetch");
+                        system ("sudo dnf install terminator");
+                        system ("flatpak install com.spotify.Client");
+                        system ("flatpak install com.google.Chrome");
+                        system ("flatpak install com.visualstudio.code");
+                        system ("sudo dnf install steam");
+                        system ("sudo dnf install lutris");
+                    }
                     else
                     {
                         printf("oops wrong choice");
@@ -197,6 +279,11 @@ int main()
                 printf("3. vscode\n");
                 printf("4. spotify\n");
                 printf("5. chrome\n");
+                printf("6. Discord\n");
+                printf("7. steam\n");
+                printf("8. Epic Games Launcher\n");
+                printf("9. OBS Studio\n");
+                printf("0. All of the above\n");
                 scanf("%d",&wp);
                 if (w == 1)
                 {
@@ -233,6 +320,44 @@ int main()
                         //chrome
                         printf("Installing chrome, please enter your password if prompted\n");
                         system ("winget install Google.Chrome -e");
+                    }
+                    else if (wp == 6)
+                    {
+                        //discord
+                        printf("Installing Discord, please enter your password if prompted\n");
+                        system ("winget install Discord.Discord -e");
+                    }
+                    else if (wp == 7)
+                    {
+                        //steam
+                        printf("Installing Steam, please enter your password if prompted\n");
+                        system ("winget install Valve.Steam -e");
+                    }
+                    else if (wp == 8)
+                    {
+                        //Epic games Launcher
+                        printf("Installing Epic games Launcher, please enter your password if prompted\n");
+                        system ("winget install EpicGames.EpicGamesLauncher -e");
+                    }
+                    else if (wp == 9)
+                    {
+                        //OBS Studio
+                        printf("Installing OBS Studio, please enter your password if prompted\n");
+                        system ("winget install OBSProject.OBSStudio -e");
+                    }
+                    else if (wp == 0)
+                    {
+                        //All of the above
+                        printf("Installing All of the above programs (this may take some time), please enter your password if prompted\n");
+                        system ("winget install Mozilla.Firefox -e");
+                        system ("winget install Notepad++.Notepad++ -e");
+                        system ("winget install Microsoft.VisualStudioCode -e");
+                        system ("winget install Spotify.Spotify -e");
+                        system ("winget install Google.Chrome -e");
+                        system ("winget install Discord.Discord -e");
+                        system ("winget install Valve.Steam -e");
+                        system ("winget install EpicGames.EpicGamesLauncher -e");
+                        system ("winget install OBSProject.OBSStudio -e");
                     }
                     else
                     {
