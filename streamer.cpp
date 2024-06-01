220d35c988030bfd991f783c7ea3b75e3384bb29
Rishikesh G <rishikesh.giridhar@outlook.com>
2024-06-01 21:45:02 +0530

    Streamer debug

--- streamer.cpp
+++ streamer.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include <socket.h> // for socket programming
+#ifdef _WIN32
+#include <winsock2.h>
+#include "vulkan_ren.cpp"
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
 
 class Streamer {
+private:
+    int socket_fd;
+    struct sockaddr_in server_addr;
+
 public:
-    void init() {
+    void init(const std::string& ip, int port) {
         // Initialize streaming socket
-        // Create a TCP or UDP socket
+#ifdef _WIN32
+        WSADATA wsaData;
+        WSAStartup(MAKEWORD(2,2), &wsaData);
+#endif
+        // Create a TCP socket
+        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (socket_fd == -1) {
+            std::cerr << "Failed to create socket." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(port);
+        server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+        // Connect to the server
+        if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+            std::cerr << "Connection to server failed." << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
 
-    void startStreaming(ScreenCapture capture, VulkanRenderer renderer, Encoder encoder) {
+    void startStreaming(ScreenCapture& capture, VulkanRenderer& renderer, Encoder& encoder) {
         while (true) {
             // Capture a frame
             void* frame;
@@ -20,10 +50,18 @@ public:
             renderer.renderFrame(frame);
 
             // Encode the frame using the encoder
-            encoder.encodeFrame(frame);
+            std::vector<unsigned char> encodedData;
+            encoder.encodeFrame(frame, encodedData);
 
             // Send the encoded frame over the network
-            // Use the socket to send the encoded frame
+            if (send(socket_fd, encodedData.data(), encodedData.size(), 0) < 0) {
+                std::cerr << "Failed to send data." << std::endl;
+                break;
+            }
         }
+        close(socket_fd);
+#ifdef _WIN32
+        WSACleanup();
+#endif
     }
 };
